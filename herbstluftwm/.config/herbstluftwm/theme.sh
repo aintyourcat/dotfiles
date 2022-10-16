9bdb365f854c41876af690f449341762d19a325c
aintyourcat <sqrt49is7@protonmail.com>
2022-10-16 17:39:02 +0800

    hlwm: update theme

--- herbstluftwm/.config/herbstluftwm/theme.sh
+++ herbstluftwm/.config/herbstluftwm/theme.sh
@@ -1,6 +1,6 @@
 herbstclient attr theme.reset 1
 
-herbstclient set frame_border_active_color "$DARK_CYAN" 
+herbstclient set frame_border_active_color "$GREY" 
 herbstclient set frame_border_normal_color "$DARK_GREY"
 herbstclient set frame_border_width 1
 herbstclient set frame_gap 10
@@ -9,7 +9,7 @@ herbstclient set frame_bg_normal_color "#00000000"
 herbstclient set frame_bg_transparent true
 herbstclient set smart_frame_surroundings true
 
-herbstclient set window_gap 2
+herbstclient set window_gap 5
 
 herbstclient attr theme.title_height 20
 herbstclient attr theme.title_when multiple_tabs
@@ -18,7 +18,7 @@ herbstclient attr theme.title_depth 8
 herbstclient attr theme.title_align center
 herbstclient attr theme.title_color "$BACKGROUND"
 
-herbstclient attr theme.border_width 8
+herbstclient attr theme.border_width 3
 herbstclient attr theme.normal.color "$DARK_GREY"
-herbstclient attr theme.active.color "$CYAN"
+herbstclient attr theme.active.color "$GREY"
 herbstclient attr theme.urgent.color "$RED"
