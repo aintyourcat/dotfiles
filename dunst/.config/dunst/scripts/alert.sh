8382d0b19d29b6d72abfd7def764ed7330e34853
aintyourcat <sqrt49is7@protonmail.com>
2022-10-30 18:10:38 +0800

    dunst - `alert.sh`: update sound_path

--- dunst/.config/dunst/scripts/alert.sh
+++ dunst/.config/dunst/scripts/alert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sound_path="$XDG_DATA_HOME/sounds/linux-a11y/stereo/complete.oga"
+sound_path="$XDG_DATA_HOME/sounds/linux-a11y/stereo/message.oga"
 
 mpv --no-terminal --volume=50 "$sound_path"
