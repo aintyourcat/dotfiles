a99a27ce562bab73570131d0426cbeb8bfdb10c0
aintyourcat <sqrt49is7@protonmail.com>
2022-10-30 18:12:56 +0800

    dunst - `alert.sh`: don't play sound on MPD track's playing/paused notification

--- dunst/.config/dunst/scripts/alert.sh
+++ dunst/.config/dunst/scripts/alert.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+appname="$1"
+body="$3"
 sound_path="$XDG_DATA_HOME/sounds/linux-a11y/stereo/message.oga"
+
+case $appname in
+    "Music Player Daemon")
+        case $body in
+            Disconnected|Reconnected) ;;
+            *) exit ;;
+        esac
+        ;;
+    *) ;;
+esac
 
 mpv --no-terminal --volume=50 "$sound_path"
