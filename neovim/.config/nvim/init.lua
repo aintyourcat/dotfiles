ea66cf36d4a9646e42e0bde36ce988676700d8a6
aintyourcat <sqrt49is7@protonmail.com>
2023-02-11 19:34:40 +0800

    nvim: update indent-blankline's config

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -170,6 +170,8 @@ require('Comment').setup()
 require('indent_blankline').setup {
   char = '┊',
   show_trailing_blankline_indent = false,
+  show_current_context = true,
+  show_current_context_start = true,
 }
 
 -- Gitsigns
