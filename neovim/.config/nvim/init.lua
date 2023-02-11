90f9ace706f39d9dc4d8e15011646b987d229bf1
aintyourcat <sqrt49is7@protonmail.com>
2023-02-11 21:05:08 +0800

    nvim: replace onedark colorscheme with iceberg

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -55,7 +55,7 @@ require('packer').startup(function(use)
   use 'tpope/vim-rhubarb'
   use 'lewis6991/gitsigns.nvim'
 
-  use 'navarasu/onedark.nvim' -- Theme inspired by Atom
+  use 'cocopon/iceberg.vim' -- Bluish color scheme for Vim and Neovim
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
@@ -127,7 +127,7 @@ vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
 vim.o.termguicolors = true
-vim.cmd [[colorscheme onedark]]
+vim.cmd [[colorscheme iceberg]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -163,7 +163,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 require('lualine').setup {
   options = {
     icons_enabled = false,
-    theme = 'onedark',
+    theme = 'iceberg',
     component_separators = '|',
     section_separators = '',
   },
