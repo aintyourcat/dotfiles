af0841a9edff81b36ea40192cd70dcc6b353ea1f
aintyourcat <sqrt49is7@protonmail.com>
2023-02-11 18:39:32 +0800

    nvim: enable some language servers

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -337,11 +337,12 @@ end
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
-  -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
-  -- rust_analyzer = {},
-  -- tsserver = {},
+  bashls = {},
+  cssls = {},
+  html = {},
+  intelephense = {},
+  tsserver = {},
+  solargraph = {},
 
   sumneko_lua = {
     Lua = {
