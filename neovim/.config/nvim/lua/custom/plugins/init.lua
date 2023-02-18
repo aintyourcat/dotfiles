0101c4e9c15a760896442b3b2d8df8cfb9c47ccd
aintyourcat <sqrt49is7@protonmail.com>
2023-02-18 22:09:45 +0800

    nvim: add "jose-elias-alvarez/null-ls.nvim", and  "mattn/emmet-vim" plugins

--- neovim/.config/nvim/lua/custom/plugins/init.lua
+++ neovim/.config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,36 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function()
+      local null_ls = require("null-ls")
+
+      return {
+        debug = true,
+        default_timeout = 10000,
+        sources = {
+          null_ls.builtins.formatting.blade_formatter,
+          null_ls.builtins.formatting.prettier.with({
+            filetypes = {
+              'javascript',
+              'json',
+              'html',
+              'css',
+              'php',
+            }
+          }),
+          null_ls.builtins.diagnostics.php,
+          null_ls.builtins.formatting.shfmt,
+          null_ls.builtins.diagnostics.shellcheck,
+          null_ls.builtins.diagnostics.erb_lint,
+          null_ls.builtins.formatting.erb_format,
+        },
+      }
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  "mattn/emmet-vim",
+}
