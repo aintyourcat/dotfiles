c1ff25c355b28372833082ff31535fe8308dca24
aintyourcat <sqrt49is7@protonmail.com>
2023-03-12 19:50:14 +0800

    nvim: lazy-load telescope.nvim, undotree, nnn.nvim and ssr.nvim on key mapping

--- neovim/.config/nvim/after/plugin/keymaps.lua
+++ neovim/.config/nvim/after/plugin/keymaps.lua
@@ -1,14 +1,8 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[G]it [S]tatus' })
 
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = '[U]ndotree toggle'})
-
-vim.keymap.set('n', '<leader>ne', '<Cmd>NnnExplorer %:p:h<CR>', { desc = '[N]nn[E]xplorer (cwd)' })
-vim.keymap.set('n', '<leader>np', vim.cmd.NnnPicker, { desc = '[N]nn[P]icker' })
-
 vim.keymap.set('v', '>', '>gv', { desc = 'Shift selection rightwards' })
 vim.keymap.set('v', '<', '<gv', { desc = 'Shift selection leftwards' })
 
-vim.keymap.set({ 'n', 'x' }, '<leader>sr', function() require('ssr').open() end)
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -266,6 +266,89 @@ return {
       -- Enable telescope fzf native, if installed
       pcall(require('telescope').load_extension, 'fzf')
     end,
+    keys = {
+      {
+        '<leader>?',
+        function() require('telescope.builtin').oldfiles() end,
+        desc = '[?] Find recently opened files'
+      },
+
+      {
+        '<leader><space>',
+        function() require('telescope.builtin').buffers() end,
+        desc = '[ ] Find existing buffers'
+      },
+
+      {
+        '<leader>/',
+        function()
+          -- You can pass additional configuration to telescope to change theme, layout, etc.
+          require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+            winblend = 10,
+            previewer = false,
+          })
+        end,
+        desc = '[/] Fuzzily search in current buffer'
+      },
+
+      {
+        '<leader>sf',
+        function()
+          require('telescope.builtin').find_files({ hidden = true, cwd = vim.fn.expand('%:p:h') })
+        end,
+        desc = '[S]earch [F]iles'
+      },
+
+      {
+        '<leader>gf',
+        function() require('telescope.builtin').git_files() end,
+        desc = '[G]it [F]iles'
+      },
+
+      {
+        '<leader>sh',
+        function() require('telescope.builtin').help_tags() end,
+        desc = '[S]earch [H]elp'
+      },
+
+      {
+        '<leader>sw',
+        function() require('telescope.builtin').grep_string() end,
+        desc = '[S]earch current [W]ord'
+      },
+
+      {
+        '<leader>sg',
+        function() require('telescope.builtin').live_grep() end,
+        desc = '[S]earch by [G]rep (cwd)'
+      },
+
+      {
+        '<leader>sG',
+        function()
+          local git_root = vim.fn.systemlist('git rev-parse --show-toplevel')
+
+          if vim.v.shell_error == 0 then
+            local opts = {
+              cwd = git_root[1],
+              additional_args = { '-.' }
+            }
+            require("telescope.builtin").live_grep(opts)
+          else
+            print 'Not a git repository.'
+          end
+        end,
+        desc = '[S]earch by [G]rep (git root)'
+      },
+
+      {
+        '<leader>sd',
+        function() require('telescope.builtin').diagnostics() end,
+        desc = '[S]earch [D]iagnostics'
+      },
+
+      { '<leader>sR', '<cmd>Telescope resume<cr>', desc = '[S]earch [R]esume' }
+    },
   },
 
 	-- Fuzzy Finder Algorithm which requires local dependencies to be built.
@@ -404,7 +487,12 @@ return {
 		end,
 	},
 
-	'mbbill/undotree',
+  {
+    'mbbill/undotree',
+    keys = {
+      { '<leader>u', vim.cmd.UndotreeToggle, desc = '[U]ndotree toggle' }
+    }
+  },
 
 	{
 		"luukvbaal/nnn.nvim",
@@ -425,7 +513,11 @@ return {
 					{ "<C-e>", nnn.builtin.populate_cmdline }, -- populate cmdline (:) with file(s)
 				}
 			}
-		end
+		end,
+		keys = {
+			{ '<leader>ne', '<Cmd>NnnExplorer %:p:h<CR>', desc = '[N]nn[E]xplorer (cwd)' },
+			{ '<leader>nE', '<Cmd>NnnExplorer<CR>',       desc = '[N]nn[E]xplorer (root)' },
+		}
 	},
 
 	{
@@ -449,7 +541,20 @@ return {
 		opts = {},
 	},
 
-	'cshuaimin/ssr.nvim',
+
+  {
+    'cshuaimin/ssr.nvim',
+    keys = {
+      {
+        '<leader>sr',
+        function()
+          require('ssr').open()
+        end,
+        mode = { 'n', 'x' },
+        desc = '[S]tructural Search and [R]eplace',
+      }
+    }
+  },
 
 	'RRethy/vim-illuminate',
 
