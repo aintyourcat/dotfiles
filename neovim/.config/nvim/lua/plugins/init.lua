e9c3ed6bce71d84546897510332fe05885bb9eb6
aintyourcat <sqrt49is7@protonmail.com>
2023-04-16 14:56:36 +0800

    nvim: remove telescope.nvim's '<leader>/' mapping

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -332,18 +332,6 @@ return {
       },
 
       {
-        '<leader>/',
-        function()
-          -- You can pass additional configuration to telescope to change theme, layout, etc.
-          require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-          })
-        end,
-        desc = '[/] Fuzzily search in current buffer'
-      },
-
-      {
         '<leader>sf',
         function()
           require('telescope.builtin').find_files({ hidden = true, cwd = vim.fn.expand('%:p:h') })
