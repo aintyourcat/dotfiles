c64e253b0a5d93d6b1f8700e59d54b0d7c50db91
aintyourcat <sqrt49is7@protonmail.com>
2025-01-08 20:02:54 +0800

    nvim: keep fundamental keymaps only

--- neovim/.config/nvim/after/plugin/keymaps.lua
+++ neovim/.config/nvim/after/plugin/keymaps.lua
@@ -3,20 +3,6 @@ vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = '[G]it [S]tatus' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Shift selection rightwards' })
 vim.keymap.set('v', '<', '<gv', { desc = 'Shift selection leftwards' })
 
--- Keymaps for better default experience
--- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-
--- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
 vim.keymap.set('n', '[b', '<cmd>bprevious<cr>')
 vim.keymap.set('n', ']b', '<cmd>bnext<cr>')
 vim.keymap.set('n', '[B', '<cmd>bfirst<cr>')
